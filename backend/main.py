73355ea6ef009e39a713ad05f3dff9871c504df1
Aryan-44 <155160031+Aryan-44@users.noreply.github.com>
2026-01-07 15:36:32 +0530

    changed port number

--- backend/main.py
+++ backend/main.py
@@ -79,4 +79,4 @@ async def vectorize_query(search: SearchQuery):
     return {"embedding": []}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
